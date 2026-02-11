b6d4d4a104c974087b1c756aba60b1343e86261f
Kunal Gaur <kg@monosolutions.com>
2026-02-11 22:58:38 +0530

    Docker setup

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,12 @@ import { GlobalZodValidationPipe } from './helpers/pipes';
 import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalFilters(new ApiExceptionFilter());
-  app.useGlobalInterceptors(new SuccessResponseInterceptor());
-//   app.useGlobalPipes(new GlobalZodValidationPipe(app.get(Reflector)));
+    app.useGlobalFilters(new ApiExceptionFilter());
+    app.useGlobalInterceptors(new SuccessResponseInterceptor());
 
-
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
